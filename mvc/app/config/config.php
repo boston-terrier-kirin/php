5123df2d4279e97845f0477aa0bdd92af294e3a8
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-07-04 10:24:07 +0900

    MVC

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+define("APPROOT", dirname(dirname(__FILE__)));
+define("URLROOT", "http://localhost:8090/mvc");
+define("SITENAME", "MVC");
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "php_mvc");
+define("DB_USER", "php_mvc");
+define("DB_PASS", "php_mvc");
