abccd3f44e6b9651f04edc6a3e807caa61941b26
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-11-12 09:04:05 +0900

    11_curl

--- 11_curl/json_server.php
+++ 11_curl/json_server.php
@@ -1,4 +1,5 @@
 <?php
+// POST
 $ch = curl_init();
 
 $payload = json_encode([
@@ -21,5 +22,24 @@ $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
 echo $status. "\n";
 echo $res;
-
 curl_close($ch);
+echo "-----\n";
+
+// GET
+$ch = curl_init();
+$options = [
+    CURLOPT_URL => "https://jsonplaceholder.typicode.com/posts/1",
+    CURLOPT_RETURNTRANSFER => true,
+];
+
+curl_setopt_array($ch, $options);
+
+$res = curl_exec($ch);
+$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+// Warning: Array to string conversion になるから変な値が返ってきているのかと思いきや、単純に配列をechoできないだけだった。
+// echo json_decode($res, true);
+
+$data = json_decode($res, true);
+print_r($data);
+print_r($data["title"]);
