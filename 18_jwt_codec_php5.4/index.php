83aa3aabf0d2831ad2b025111b8d3971bf23e168
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-11-19 15:53:14 +0900

    18_jwt_codec_php5.4

---
+++ 18_jwt_codec_php5.4/index.php
@@ -0,0 +1,20 @@
+<?php
+require "JWTCodec.php";
+
+$payload = ["id" => 123];
+$codec = new JWTCodec("5A7234753778214125442A472D4A614E645267556B58703273357638792F423F");
+
+$token = $codec->encode($payload);
+
+$decoded_payload = $codec->decode($token);
+
+echo $token;
+echo "<br />-----<br />";
+print_r($decoded_payload);
+echo "<br />-----<br />";
+
+try {
+    $decoded_payload = $codec->decode($token . "a");
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
